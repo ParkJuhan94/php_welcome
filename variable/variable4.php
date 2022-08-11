c37d6b0771b0e044ff3dcbd8b4649c30f56f5bf6
JuHan <1004sunye@naver.com>
2022-08-11 12:49:26 +0900

    0811

---
+++ variable/variable4.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<?php
+
+// 변수를 통해서 효율적으로 프로그래밍 유지 보수 가능해짐
+$a = 100;
+$a = $a + 10;
+print $a.'<br />';
+$a = $a / 10;
+print $a.'<br />';
+$a = $a - 10;
+print $a.'<br />';
+$a = $a * 10 ;    
+print $a.'<br />';
+?>
+</body>
+</html>
